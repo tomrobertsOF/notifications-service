9e72393d1325316accb32ae0d85b692718b6886f
Tom Roberts <troberts@openfin.co>
2020-01-15 10:37:51 +0000

    First pass of feeds view

--- src/provider/view/containers/NotificationCenterApp/NotificationsFeedsPanel/FeedsPanel.tsx
+++ src/provider/view/containers/NotificationCenterApp/NotificationsFeedsPanel/FeedsPanel.tsx
@@ -1,33 +1,30 @@
 import * as React from 'react';
-import {RouteComponentProps} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {RouteComponentProps} from 'react-router';
 import {MemoryHistory} from 'history';
 
 import {RootState} from '../../../../store/State';
 import {Header} from '../../../components/Header/Header';
-import {SettingsView} from '../../../components/SettingsView/SettingsView';
 import {ClassNameBuilder} from '../../../utils/ClassNameBuilder';
-import {Icon} from '../../../components/Icon/Icon';
-import SettingsIcon from '../../../../../../res/provider/ui/image/shapes/Settings.svg';
+import {FeedsView} from '../../../components/FeedsView/FeedsView';
 
-import * as Styles from './SettingsPanel.module.scss';
+import * as Styles from './FeedsPanel.module.scss';
 
 interface Props extends RouteComponentProps {
 
 }
 
-const SettingsPanelComponent: React.FC<Props> = (props) => {
+const FeedsPanelComponent: React.FC<Props> = (props) => {
     const {history} = props;
 
     return (
         <div className="notification-center">
             <Header history={history as MemoryHistory}>
                 <div className={ClassNameBuilder.join(Styles, 'strip-content')}>
-                    <span>Settings</span>
-                    <Icon className={ClassNameBuilder.join(Styles, 'icon')} src={SettingsIcon} />
+                    <span>Notification Feeds</span>
                 </div>
             </Header>
-            <SettingsView {...props} />
+            <FeedsView />
         </div>
     );
 };
@@ -35,4 +32,4 @@ const SettingsPanelComponent: React.FC<Props> = (props) => {
 const mapStateToProps = (state: RootState) => ({
 });
 
-export const SettingsPanel = connect(mapStateToProps)(SettingsPanelComponent);
+export const FeedsPanel = connect(mapStateToProps)(FeedsPanelComponent);
